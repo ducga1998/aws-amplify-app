6ea21abb1962d5d58d6cce3e08e2b9d1d4a2fa18
Ducnm <duc2820@gmail.com>
2019-06-06 21:04:50 +0700

    init app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import { Auth } from 'aws-amplify'
 
 function App() {
+  useEffect(() => {
+    Auth.currentAuthenticatedUser()
+      .then(user => console.log({ user }))
+      .catch(error => console.log({ error }))
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
